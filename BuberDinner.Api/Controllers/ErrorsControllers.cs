ff57c4538ee822b6dba15f3c606e5ae95d604208
MalcolmMello <malcolmmellolima@gmail.com>
2024-01-21 12:24:54 -0300

    Global Error Handling

---
+++ BuberDinner.Api/Controllers/ErrorsControllers.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    public class ErrorsControllers : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+            return Problem(title: exception?.Message);
+        }
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,16 +1,25 @@
+// using BuberDinner.Api.Filters;
+// using BuberDinner.Api.Middleware;
+using BuberDinner.Api.Errors;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
+    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    //app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
